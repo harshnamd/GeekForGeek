ff0e78570a44d80615aa806b1ec0f38a18044247
Harsh Namdev <163432385+harshnamd@users.noreply.github.com>
2024-12-04 21:22:25 +0530

    Create Strings_Rotations_of_Each_Other.py

---
+++ Strings_Rotations_of_Each_Other.py
@@ -0,0 +1,9 @@
+class Solution:
+    def areRotations(self,s1,s2):
+        if len(s1)!=len(s2):
+            return False
+        s1+=s1
+        if s2 in s1:
+            return True
+        else:
+            return False
